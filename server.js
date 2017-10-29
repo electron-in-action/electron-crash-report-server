bf70b580a6352716ea84f68e80025e21c3d871fc
Steve Kinney <hello@stevekinney.net>
2017-10-29 14:07:25 -0600

    Add exceptions

--- server.js
+++ server.js
@@ -1,27 +1,47 @@
 const express = require('express');
 const multer = require('multer');
 const bodyParser = require('body-parser');
+const uuid = require('uuid');
+const writeFile = require('write-file');
 const path = require('path');
-const fs = require('fs');
 const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const crashesPath = path.join(__dirname, 'crashes');
+const exceptionsPath = path.join(__dirname, 'uncaughtexceptions');
 
 const upload = multer({
   dest: crashesPath,
 }).single('upload_file_minidump');
 
 app.post('/crashreports', upload, (request, response) => {
-  const body = { ...request.body, filename: request.file.filename };
+  const body = {
+    ...request.body,
+    filename: request.file.filename,
+    date: new Date(),
+  };
   const filePath = `${request.file.path}.json`;
-  const crashReport = JSON.stringify(body);
+  const report = JSON.stringify(body);
 
-  fs.writeFile(filePath, crashReport, error => {
-    if (error) return console.error('Error Saving', crashReport);
-    console.log('Crash Saved', filePath, crashReport);
+  writeFile(filePath, report, error => {
+    if (error) return console.error('Error Saving', report);
+    console.log('Crash Saved', filePath, report);
+  });
+
+  response.end();
+});
+
+app.post('/uncaughtexceptions', (request, response) => {
+  const filePath = path.join(exceptionsPath, `${uuid()}.json`);
+  const report = JSON.stringify({ ...request.body, date: new Date() });
+
+  writeFile(filePath, report, error => {
+    if (error) return console.error('Error Saving', report);
+    console.log('Exception Saved', filePath, report);
   });
 
   response.end();
